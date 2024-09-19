4b9ce225b977b8e332d9777e26a178d2f0b512da
eheka78 <123297062+eheka78@users.noreply.github.com>
2024-09-19 16:20:02 +0900

    Title: 2차원 최대 증가 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240919/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 240919/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -9,7 +9,7 @@ int main() {
     // input
     cin >> n >> m;
     for(int i{0};i<n;i++){
-        for(int j{0};j<n;j++){
+        for(int j{0};j<m;j++){
             cin >> arr[i][j];
         }
     }
@@ -23,26 +23,6 @@ int main() {
     dp[0][0] = 1;
 
     // process
-    /*for(int i{1};i<n;i++){
-        for(int j{0};j<i;j++){
-            if(dp[0][j] == -1) continue;
-
-            if(arr[0][i] > arr[0][j]){
-                dp[0][i] = max(dp[0][i], dp[0][j] + 1);
-            }
-        }
-    }
-
-    for(int i{1};i<m;i++){
-        for(int j{0};j<i;j++){
-            if(dp[j][0] == -1) continue;
-
-            if(arr[i][0] > arr[j][0]){
-                dp[i][0] = max(dp[i][0], dp[j][0] + 1);
-            }
-        }
-    }*/
-
     for(int i{1}; i<n; i++){
         for(int j{1}; j<m; j++){
             for(int k{0}; k<i; k++){
@@ -57,19 +37,19 @@ int main() {
         }
     }
 
-    /*for(int i{0};i<n;i++){
-        for(int j{0};j<m;j++){
-            cout << dp[i][j] << " ";
-        }
-        cout << '\n';
-    }*/
+    // for(int i{0};i<n;i++){
+    //     for(int j{0};j<m;j++){
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << '\n';
+    // }
 
     int result{0};
     for(int i{0};i<n;i++){
         for(int j{0};j<m;j++){
             result = max(result, dp[i][j]);
         }
-    }
+    }   
     
     cout << result << '\n';
 
